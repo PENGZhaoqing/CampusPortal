cc910956976fd25123e2afa643e227d0f7629690
peng <ppp8300885@gmail.com>
2016-10-10 00:41:07 +0800

    fix s3 bug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['S3_KEY'],                        # required
       aws_secret_access_key: ENV['S3_SECRET'],                        # required
-      # region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+      region:                'ap-southeast-1',                  # optional, defaults to 'us-east-1'
       # host:                  'https://s3-us-west-2.amazonaws.com/campus-portal',             # optional, defaults to nil
       # endpoint:               'https://campus-portal.s3-ap-southeast-1.amazonaws.com'
   }
