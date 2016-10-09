9f1700d1972de828f0f9e45efbf915aeebf02f8f
peng <ppp8300885@gmail.com>
2016-10-10 00:30:38 +0800

    debug s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       # host:                  'https://s3-us-west-2.amazonaws.com/campus-portal',             # optional, defaults to nil
       # endpoint:               'https://campus-portal.s3-ap-southeast-1.amazonaws.com'
   }
-  config.fog_directory  = 'campus-portal'                          # required
+  # config.fog_directory  = 'campus-portal'                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
